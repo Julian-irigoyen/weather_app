5151c74c06b4e5dc3aefb1c02d89b8a882d50b33
unknown <polxr27@gmail.com>
2023-12-29 07:40:24 -0600

    Server functions test

--- serverless/get_weather.js
+++ serverless/get_weather.js
@@ -1,10 +1,11 @@
 const fetch = require("node-fetch");
+
 const { WEATHER_API_KEY } = process.env;
 
 exports.handler = async (event, context) => {
   const params = JSON.parse(event.body);
   const { lat, lon, units } = params;
-  const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly,alert&units=${units}&appid=${WEATHER_API_KEY}`;
+  const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly,alerts&units=${units}&appid=${WEATHER_API_KEY}`;
   try {
     const weatherStream = await fetch(url);
     const weatherJson = await weatherStream.json();
